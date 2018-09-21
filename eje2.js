6d451b5c8872d0fa315d59a1e9af30df8bbca00e
dagr27 <00118717@uca.edu.sv>
2018-09-21 03:02:29 -0600

    Subiendo 2

---
+++ eje2.js
@@ -0,0 +1,10 @@
+var cantidad = prompt("Ingrese la cantidad de palabras que desea ingresar:");
+function ordenar(cantidad){
+    texto = new Array(cantidad);
+    for(var i = 0; i < cantidad; i++){
+        var mensaje = prompt("Ingrese la cantidad de palabras que desea ingresar:");
+        palabra.push(texto);
+    }
+        alert(palabra.sort());
+    }
+ordenar(cantidad);
